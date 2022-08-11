38bd0cf26b9428ca2c34a3a67f9fa61751f2827d
malav-prama <malav.thakker@prama.ai>
2022-08-11 12:55:54 -0700

    Update InventoryLogic.java

--- src/main/java/com/example/inventoryservice/logic/InventoryLogic.java
+++ src/main/java/com/example/inventoryservice/logic/InventoryLogic.java
@@ -3,6 +3,7 @@ package com.example.inventoryservice.logic;
 import com.example.inventoryservice.dao.InventoryDao;
 import com.example.inventoryservice.model.Inventory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.example.inventoryservice.model.*;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,14 +29,21 @@ public class InventoryLogic {
         return inventoryDao.getInventoryById(productId);
     }
 
-    public void updateInventory(updateInventory updateInventory)
+    public ResponseEntity<?> updateInventory(updateInventory updateInventory)
     {
-        Inventory inventory=new Inventory();
-        inventory.setProductId(updateInventory.getProductId());
+        Inventory inventory=inventoryDao.getInventoryById(updateInventory.getProductId());
+            
         int currentQty=inventory.getTotalQuantity();
-        int updateQTY=currentQty-updateInventory.getProductQty();
-        inventory.setTotalQuantity(updateQTY);
-        inventoryDao.updateInventory(inventory);
+        if(currentQty>0) {
+            int updateQTY = currentQty - updateInventory.getProductQty();
+            inventory.setTotalQuantity(updateQTY);
+            inventoryDao.updateInventory(inventory);
+            return ResponseEntity.status(200).build();
+        }
+        else {
+            return ResponseEntity.status(406).build();
+        }
+
 
     }
 
